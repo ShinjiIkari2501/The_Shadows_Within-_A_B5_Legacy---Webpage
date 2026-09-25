cd66cefa4bae60ead4fdcda77ef2132c359396ec
Daniel Baedorf <ikari.kun01@gmail.com>
2026-09-25 15:37:15 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,11 +1,10 @@
 <?php 
-// 1. Bindet die Login-Verarbeitung direkt auf der Startseite ein
-require_once 'Login.php';
 
-// 2. Die Layout-Zentrale laden (Sie startet auch automatisch die Session)
+
+// 1. Die Layout-Zentrale laden (Sie startet auch automatisch die Session)
 require_once 'Includes/layout.php'; 
 
-// 3. Den Zwischenspeicher für den HTML-Inhalt aktivieren
+// 2. Den Zwischenspeicher für den HTML-Inhalt aktivieren
 ob_start(); 
 ?>
 
